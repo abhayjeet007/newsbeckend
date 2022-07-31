2f61f33de56ff17e24d5ec8b07bf7a5c0f9854f8
abhayjeet007 <105332985+abhayjeet007@users.noreply.github.com>
2022-07-31 14:37:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors=require('cors')
 require('dotenv').config();
 connectToMongo();
 const app = express()
-const port = 5000||process.env.PORT;
+const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 app.get('/',(req,res)=>{res.send("gdkjhl")})
